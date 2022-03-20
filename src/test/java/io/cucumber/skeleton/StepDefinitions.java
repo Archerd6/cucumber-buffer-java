8f0fd550ab298551df0ed088f22e62e886e11857
Archerd6 <archerd@uma.es>
2022-03-20 12:47:49 +0100

    2.0

--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -8,45 +8,76 @@ import io.cucumber.java.en.When;
 
 public class StepDefinitions
 {
-	private BufferAcotado<String> buffer;
-	private int numeroElementosContenidos = 0;
-	boolean bufferLleno = false;
-	String primerElemento;
-	@Given("I have a buffer with a capacity of {int}")
-	public void i_have_a_buffer_with_a_capacity_of(Integer int1)
+	private BufferAcotado<String> privateBuffer; // La instancia del buffer que voy a probar
+
+	String primerElemento;                       // Variable para comprobar el primer elemento que se insertó
+	private int bufferSize;                      // Variable para comprobar el tamaño que debería tener el buffer
+	boolean bufferLleno;                         // Variable para comprobar si debería estar lleno el buffer
+	
+	@Given("I have a <string>buffer with a capacity of {int}")
+	public void i_have_a_string_buffer_with_a_capacity_of(Integer int1)
 	{
-	    // Write code here that turns the phrase above into concrete actions
-		buffer = new BufferAcotado<String>(int1);
+		privateBuffer = new BufferAcotado<String>(int1); // Tipo String <String>
 	}
 	@When("I add the first element {string}")
 	public void i_add_the_first_element(String string)
 	{
 	    try
 	    {
-	    	buffer.put(string);
-		    numeroElementosContenidos++;
+	    	privateBuffer.put(string);
+		    this.bufferSize++;                           // Se debería aumentar el tamaño
 	    } catch (IllegalStateException e)
 	    {
 	    	bufferLleno = true;
 	    }
-	    primerElemento = string;
+	    primerElemento = string;                         // Se guarda el primer string
 	}
-	@When("I add an {string}")
+	@When("I add a {string}")
 	public void i_add_an(String string)
 	{
 	    try
 	    {
-	    	buffer.put(string);
-		    numeroElementosContenidos++;
+	    	privateBuffer.put(string);
+		    bufferSize++;                                // Se debería aumentar el tamaño
 	    } catch (IllegalStateException e)
 	    {
 	    	bufferLleno = true;
 	    }
 	}
-	@Then("The first element to get should be the first I have put in the buffer")
-	public void the_first_element_to_get_should_be_the_first_i_have_put_in_the_buffer()
+	@Then("Get should be the first string")
+	public void get_should_be_the_first_string()
 	{
-		Assertions.assertEquals(primerElemento, buffer.get());
+		Assertions.assertEquals(primerElemento, privateBuffer.get());
+	}
+	
+	@Then("The buffer should be empty")
+	public void the_buffer_should_be_empty()
+	{
+		 Assertions.assertTrue(privateBuffer.isEmpty());
+	}
+	
+	@Then("The size should be full")
+	public void the_size_should_be_full()
+	{
+		Assertions.assertTrue(privateBuffer.isFull());
+	}
+	
+	@Then("It is added to the buffer")
+	public void it_is_added_to_the_buffer() {
+		Assertions.assertEquals(bufferSize, privateBuffer.size());
+	}
+	
+	@Given("I have a full buffer")
+	public void i_have_a_full_buffer()
+	{
+		privateBuffer = new BufferAcotado<String>(2);
+		privateBuffer.put("first string");
+		privateBuffer.put("second string");
+	}
+	@Then("I can't add the element")
+	public void i_can_t_add_the_element()
+	{
+		Assertions.assertEquals(true, bufferLleno);
 	}
 	
 }
--- src/test/resources/io/cucumber/skeleton/Test.feature
+++ src/test/resources/io/cucumber/skeleton/Test.feature
@@ -1,8 +1,37 @@
 Feature: TestGlobal
 
+   Scenario Outline: Put
+    Given I have a <string>buffer with a capacity of 5
+    When I add a "element"
+    Then It is added to the buffer
+		
+    Examples:
+      | element |
+      |   one   |
+      |   two   |
+      |   tree  |
+      |   four  |
+      
+   Scenario: Size
+    Given I have a <string>buffer with a capacity of 3
+    When I add a "first string"
+    When I add a "second string"
+    When I add a "third string"
+    Then The size should be full
+   
    Scenario: Get
-    Given I have a buffer with a capacity of 3
-    When I add the first element "element1"
-    When I add an "element2"
-    When I add an "element3"
-    Then The first element to get should be the first I have put in the buffer
+    Given I have a <string>buffer with a capacity of 8
+    When I add the first element "first string"
+    When I add a "second string"
+    When I add a "third string"
+    Then Get should be the first string
+    
+   Scenario: Emptiness
+    Given I have a <string>buffer with a capacity of 88
+    Then The buffer should be empty
+   
+   Scenario: Fullness
+    Given I have a full buffer
+    When I add a "element"
+    Then I can't add the element
+    		
